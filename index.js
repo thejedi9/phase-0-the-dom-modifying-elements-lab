d1d4721866eefbc8274a5fd79f430457e886abbc
Jede <brahmajede@gmail.com>
2021-04-08 18:44:39 -0400

    submitted incorrectly before

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('header')
+
+newHeader.nodeName = "h1"
+newHeader.id = "victory"
+newHeader.innerText = "Jede is the champion"
+
